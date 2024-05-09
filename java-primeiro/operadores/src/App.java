49dcaf621cd8770859fe9c0fa59ae55d3a2b7b36
dodofin <douglas.vilar3@gmail.com>
2024-05-09 18:09:27 -0300

    operadores

--- java-primeiro/operadores/src/App.java
+++ java-primeiro/operadores/src/App.java
@@ -1,16 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    
-        byte idade = 32;
-    
-        System.out.println(idade);
-    
-    
     }
-
-
-
-
-
 }
---
+++ java-primeiro/src/operadores.java
@@ -0,0 +1,16 @@
+public class operadores {
+
+public static void main(String[] args) {
+    
+
+    String nome1 = "Douglas";
+    String nome2 = "Alexandre";
+    String nomeCompleto = (nome1) + (nome2);
+    
+    System.out.println(nomeCompleto);
+
+
+
+}
+
+}
